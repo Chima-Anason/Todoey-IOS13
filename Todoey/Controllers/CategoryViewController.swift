52551d0a873f236f3c098eb206a63e545e801670
Chima-Anason <tblynxx@gmail.com>
2023-02-25 23:15:28 +0800

    Create Relationship between Categories and Item in Database. Closes #10

---
+++ Todoey/Controllers/CategoryViewController.swift
@@ -0,0 +1,32 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by mac on 25/02/2023.
+//  Copyright © 2023 App Brewery. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    //MARK: - TableView Data Source Method
+    
+    
+    //MARK: - TableView delegate method
+    
+    
+    //MARK: - Add New Categories
+
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+    }
+    
+}
